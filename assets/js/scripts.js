d00c7a88cd11a74b8bdd08a681079e856e41afe7
James <jameslfidlin@gmail.com>
2022-06-01 01:01:48 -0400

    2nd commit, started the api call structure

---
+++ assets/js/scripts.js
@@ -0,0 +1,45 @@
+$( "#submit_city" ).click(function() {
+
+let API_key = "52b83ade18965ec1516feaeccd8b85c8"
+let API_link = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="+API_key
+alert(API_link);
+//parameters to use 
+// city name
+// date
+// icons if available, else use font awesome
+// temperature
+// humidity
+// windspeed
+// uv index
+
+// 5 day future forecast 
+
+// dates
+// icons if available, else use font awesome
+// temperatures
+// humidity levels
+// windspeeds
+
+
+
+
+
+
+
+
+
+    fetch()
+
+    
+
+    .then({
+
+    })
+
+    .then({
+
+    })
+    
+    
+    
+  });
