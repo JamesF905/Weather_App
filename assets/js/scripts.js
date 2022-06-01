d0df07b8f91d34b401a94a485d413ebf8342dcba
James <jameslfidlin@gmail.com>
2022-06-01 04:34:00 -0400

    3rd commit, completed the api call structure, achieved successful retreival of 1 day information, moving on to 5 day

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,56 @@
-$( "#submit_city" ).click(function() {
+let API_key = "52b83ade18965ec1516feaeccd8b85c8";
 
-let API_key = "52b83ade18965ec1516feaeccd8b85c8"
-let API_link = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="+API_key
-alert(API_link);
+$( "#submit_city" ).click(function(event) {
+event.preventDefault();
+let city = $( "#city" ).val();
+let state = "ontario"; /*$( "#city" ).val();*/
+let country = "CA"; /*$( "#city" ).val();*/
+let geo_code_link = "http://api.openweathermap.org/geo/1.0/direct?q="+city+","+state+","+country+"&limit=5&appid="+API_key;
+// current api link https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+API_key;
+//one call api link https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+API_key;
+
+
+fetch(geo_code_link)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        let latitude = data[0].lat;
+        let longitude = data[0].lon;
+        let API_link = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+API_key;
+
+        console.log(data);
+        return fetch(API_link);
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        compile_API_data(data);
+    })
+
+function compile_API_data(data){
+    let time = data.current.dt;
+    let conditions = data.current.weather[0].main;
+    let icon = data.current.weather[0].icon;
+    let temp = data.current.temp;
+    let humidity = data.current.humidity;
+    let wind_speed = data.current.wind_speed;
+    let uvi = data.current.uvi;
+
+    alert("time"+time);
+    alert("conditions"+conditions);
+    alert("icon"+icon);
+    alert("temp"+temp);
+    alert("humidity"+humidity);
+    alert("wind_speed"+wind_speed);
+    alert("uvi"+uvi);
+}
+//alert("latitude: "+data[0].lat);
+//alert("longitude: "+data[0].lon);
+//let API_link = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="+API_key
+
 //parameters to use 
 // city name
 // date
@@ -18,28 +66,7 @@ alert(API_link);
 // icons if available, else use font awesome
 // temperatures
 // humidity levels
-// windspeeds
-
-
-
-
-
-
-
-
-
-    fetch()
-
-    
-
-    .then({
-
-    })
-
-    .then({
-
-    })
-    
+// windspeeds  
     
     
   });
