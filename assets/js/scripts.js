f1b6c9f3e04a5ae99c51d9adea355090ad94fb98
James <jameslfidlin@gmail.com>
2022-06-01 22:42:23 -0400

    completed most of the jquery and html structure, need to finish local storage and css styling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,36 +1,41 @@
 let API_key = "52b83ade18965ec1516feaeccd8b85c8";
 
-$( "#submit_city" ).click(function(event) {
-event.preventDefault();
-let city = $( "#city" ).val();
-let state = "ontario"; /*$( "#city" ).val();*/
-let country = "CA"; /*$( "#city" ).val();*/
-let geo_code_link = "http://api.openweathermap.org/geo/1.0/direct?q="+city+","+state+","+country+"&limit=5&appid="+API_key;
-// current api link https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+API_key;
-//one call api link https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+API_key;
+$( "#submit_city" ).click(compile());
 
 
-fetch(geo_code_link)
-    .then(function (response) {
-        return response.json();
-    })
+function compile(event){
+    event.preventDefault();
+    let city = $( "#city" ).val();
+    let state = "ontario"; /*$( "#city" ).val();*/
+    let country = "CA"; /*$( "#city" ).val();*/
+    let link = "http://api.openweathermap.org/geo/1.0/direct?q="+city+","+state+","+country+"&limit=5&appid="+API_key;
+    
+    //send first API call
+    fetch(link)
+    .then(response => response.json())
     .then(function (data) {
-        let latitude = data[0].lat;
-        let longitude = data[0].lon;
-        let API_link = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+API_key;
-
-        console.log(data);
-        return fetch(API_link);
+        // get coordinates for the city to use in the oneCall API call
+        let oneCall_link = "https://api.openweathermap.org/data/2.5/onecall?lat="+data[0].lat+"&lon="+data[0].lon+"&units=metric&appid="+API_key;
+        return fetch(oneCall_link);
     })
-    .then(function (response) {
-        return response.json();
-    })
+    .then(response => response.json())
     .then(function (data) {
-        console.log(data);
-        compile_API_data(data);
+        //compile the html for the one call
+        oneDay_forecast(data);
+        //send the link for the fiveDay API call 
+        let fiveDay_link = "https://api.openweathermap.org/data/2.5/forecast?lat="+data.lat+"&lon="+data.lon+"&units=metric&appid="+API_key;
+        return fetch(fiveDay_link);
+    })
+    .then(response => response.json())
+    .then(function (data) {
+        //compile the html for the 5 day forecast
+        fiveDay_forecast(data);
     })
 
-function compile_API_data(data){
+    console.log(coordinates);
+}
+
+function oneDay_forecast(data){
     let time = data.current.dt;
     let conditions = data.current.weather[0].main;
     let icon = data.current.weather[0].icon;
@@ -38,35 +43,19 @@ function compile_API_data(data){
     let humidity = data.current.humidity;
     let wind_speed = data.current.wind_speed;
     let uvi = data.current.uvi;
-
-    alert("time"+time);
-    alert("conditions"+conditions);
-    alert("icon"+icon);
-    alert("temp"+temp);
-    alert("humidity"+humidity);
-    alert("wind_speed"+wind_speed);
-    alert("uvi"+uvi);
 }
-//alert("latitude: "+data[0].lat);
-//alert("longitude: "+data[0].lon);
-//let API_link = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="+API_key
 
-//parameters to use 
-// city name
-// date
-// icons if available, else use font awesome
-// temperature
-// humidity
-// windspeed
-// uv index
+function fiveDay_forecast(data){
+    for(i=0; i<data.length; i++){
+        let time = data[i].dt_txt;
+        let conditions = data[i].weather[0].main;
+        let icon = data[i].weather[0].icon;
+        let temp = data[i].main.temp;
+        let humidity = data[i].main.humidity;
+        let wind_speed = data[i].wind.speed;
+    }
+}
 
-// 5 day future forecast 
-
-// dates
-// icons if available, else use font awesome
-// temperatures
-// humidity levels
-// windspeeds  
-    
-    
-  });
+function city_history(data){
+    //Store in local storage
+}
