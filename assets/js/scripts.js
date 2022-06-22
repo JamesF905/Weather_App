1ef71f3ba250cc39008f3f9b8ef3c39b709b16c5
James <jameslfidlin@gmail.com>
2022-06-22 01:01:51 -0400

    added styles to main one day section, added to scripts file to further refine the api call

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,23 @@
+
 let API_key = "52b83ade18965ec1516feaeccd8b85c8";
 let city;
+let state;
+let country;
 $( "#submit_city" ).click(compile);
 
 
 function compile(event){
     event.preventDefault();
     city = $( "#city" ).val();
-    let state = "ontario"; /*$( "#city" ).val();*/
-    let country = "CA"; /*$( "#city" ).val();*/
+    state = "Ontario"; /*$( "#city" ).val();*/
+    country = "CA"; /*$( "#city" ).val();*/
     let link = "http://api.openweathermap.org/geo/1.0/direct?q="+city+","+state+","+country+"&limit=5&appid="+API_key;
     
     //send first API call
     fetch(link)
     .then(response => response.json())
     .then(function (data) {
+        
         console.log(data);
         // get coordinates for the city to use in the oneCall API call
         let oneCall_link = "https://api.openweathermap.org/data/2.5/onecall?lat="+data[0].lat+"&lon="+data[0].lon+"&units=metric&appid="+API_key;
@@ -21,6 +25,7 @@ function compile(event){
     })
     .then(response => response.json())
     .then(function (data) {
+        
         //compile the html for the one call
         oneDay_forecast(data);
         console.log(data);
@@ -30,6 +35,7 @@ function compile(event){
     })
     .then(response => response.json())
     .then(function (data) {
+        
         //compile the html for the 5 day forecast
         fiveDay_forecast(data);
         console.log(data);
@@ -37,6 +43,7 @@ function compile(event){
 }
 
 function oneDay_forecast(data){
+    
     let time = data.current.dt;
     let conditions = data.current.weather[0].main;
     let icon = "http://openweathermap.org/img/w/"+data.current.weather[0].icon+".png";
@@ -45,8 +52,9 @@ function oneDay_forecast(data){
     let wind_speed = data.current.wind_speed;
     let uvi = data.current.uvi;
 
-    $(" #city_name ").text(city+" ("+time+")");
-    $("<img>").attr("src", icon).insertAfter($(" #city_name "));
+    $(" #city_name ").text(city);
+    $(" #state_name ").text(state+", "+country);
+    //$("<img>").attr("src", icon).insertBefore($(" #city_name "));
     oneDay_array = [uvi, humidity, wind_speed, temp];
     for(i=0;i<oneDay_array.length;i++){
         if(i== 0){
@@ -57,6 +65,7 @@ function oneDay_forecast(data){
 }
 
 function fiveDay_forecast(data){
+    
     for(i=0; i<data.length; i++){
         let time = data.list[i].dt_txt;
         let conditions = data.list[i].weather[0].main;
